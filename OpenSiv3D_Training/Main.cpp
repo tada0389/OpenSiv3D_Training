8b3a1e1d913d879a0023f66df08f0a8b68fc853c
taisei <tatata0389@gmail.com>
2020-08-05 18:33:54 +0900

    円の表示で遊んだ

--- OpenSiv3D_Training/Main.cpp
+++ OpenSiv3D_Training/Main.cpp
@@ -1,47 +1,69 @@
-﻿# include <Siv3D.hpp>
+﻿#include <Siv3D.hpp>
 
-void Main()
-{
-    // 使用する画像
-    const Image image(U"example/siv3d-kun.png");
+class CircleManager {
+public:
+    CircleManager(const double _duration, const double _interval) :
+        duration_(_duration), interval_(_interval) {
+        eclipsedTime_ = 0.0;
+        hue_ = 0;
+    }
 
-    // アルファ値 1 以上の領域を Polygon 化
-    const Polygon polygon = image.alphaToPolygon(1, false);
+    void Update() {
 
-    // テクスチャ
-    const Texture texture(image);
-
-    // 移動のオフセット
-    constexpr Vec2 pos(40, 40);
-
-    // Polygon 単純化時の基準距離（ピクセル）
-    double maxDistance = 4.0;
-
-    // 単純化した Polygon
-    Polygon simplifiedPolygon = polygon.simplified(maxDistance);
-
-    while (System::Update())
-    {
-        // 単純化した Polygon の頂点数を表示
-        ClearPrint();
-        Print << simplifiedPolygon.vertices().size() << U" vertices";
-
-        texture.draw(pos);
-
-        // 単純化した Polygon を表示
-        simplifiedPolygon.movedBy(pos)
-            .draw(ColorF(1.0, 1.0, 0.0, 0.2))
-            .drawWireframe(2, Palette::Yellow);
-
-        // 単純化した Polygon をマウスカーソルに追従させて表示
-        simplifiedPolygon.movedBy(Cursor::Pos() - image.size() / 2)
-            .draw(ColorF(0.5));
-
-        // Polygon 単純化時の基準距離を制御
-        if (SimpleGUI::Slider(U"{:.1f}"_fmt(maxDistance), maxDistance, 0.0, 30.0, Vec2(400, 40), 60, 200))
-        {
-            // スライダーに変更があれば、単純化した Polygon を新しい基準値で再作成
-            simplifiedPolygon = polygon.simplified(maxDistance);
+        // 大きさ更新
+        auto iterator = circles_.begin();
+        for (auto it = circles_.begin(); it != circles_.end(); ++it) {
+            get<0>(*it) += 200.0 * duration_ * Scene::DeltaTime();
+            if (get<0>(*it) >= 200.0) {
+                get<0>(*it) = 200.0;
+                iterator = it;
+            }
         }
+
+        if (iterator != circles_.begin()) {
+            circles_.erase(circles_.begin(), std::prev(iterator));
+        }
+
+        Print << circles_.size();
+
+        eclipsedTime_ += Scene::DeltaTime();
+
+        if (eclipsedTime_ >= interval_) {
+            eclipsedTime_ -= interval_;
+            circles_.emplace_back(0.0, HSV(hue_), Cursor::Pos());
+            hue_ += 40;
+            hue_ %= 360;
+        }
+    }
+
+    void draw() const{
+        for (const auto [r, c, p] : circles_) {
+            //Circle(Scene::Center(), r).draw(c);
+            Circle(p, r).draw(c).drawFrame(1.0, Palette::Gray);
+        }
+    }
+
+private:
+    Array<std::tuple<double, ColorF, Vec2>> circles_;
+
+    // 円を最大限までの拡大するまでの時間
+    double duration_;
+    // 次の円が表示されるまでの時間
+    double interval_;
+
+    double eclipsedTime_;
+    int hue_;
+};
+
+void Main() {
+
+    Scene::SetBackground(Palette::White);
+
+    CircleManager circleManager(0.5, 0.15);
+
+    while (System::Update()) {
+
+        circleManager.Update();
+        circleManager.draw();
     }
 }
